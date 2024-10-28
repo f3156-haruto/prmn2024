8aa8bbe117a75ce8fdf1e8d8a7461c515ac5cbfc
f3156-haruto <haruto.f31056@gmail.com>
2024-10-28 15:14:40 +0900

    プロジェクト作成

---
+++ prmn2024Fall/prmn2024Fall/src/test/java/org/example/prmn2024fall/Prmn2024FallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prmn2024fall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024FallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
