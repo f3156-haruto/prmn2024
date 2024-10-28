0f464c427ee63e8aed9bf3ef644630b73089e745
f3156-haruto <haruto.f31056@gmail.com>
2024-10-28 14:12:22 +0900

    prmn2024Fall

--- prmn2024/src/main/java/org/example/prmn2024/Prmn2024Application.java
+++ prmn2024/src/main/java/org/example/prmn2024/Prmn2024Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Prmn2024Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Prmn2024Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024Application.class, args);
+    }
 
 }
